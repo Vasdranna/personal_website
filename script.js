777fbd7a0b9bfe778d7b0b4d19068d3bd4c9b949
Vasdranna <107063361+Vasdranna@users.noreply.github.com>
2025-01-17 12:02:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/* ———————————————————————————————————————————————————————————————————————————————————— */
+/*                                     OBFUSCATE EMAIL                                  */
+/* ———————————————————————————————————————————————————————————————————————————————————— */
+const u = atob("Z2l1bGlhbm9kYW5pZWxl");
+const d = atob("eWFob28uY29t");
+const a = document.querySelector(".emailLink");
+a.href = "mailto:" + u + "@" + d;
+
+/* ———————————————————————————————————————————————————————————————————————————————————— */
+/*                                    ABOUT ME MODAL                                    */
+/* ———————————————————————————————————————————————————————————————————————————————————— */
+const openModal = document.querySelector(".openModal");
+const closeModal = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal");
+
+// Show modal
+openModal.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+// Close modal with click in close button
+closeModal.addEventListener("click", () => {
+  closeModalFn();
+});
+
+// Close modal with Esc key
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" || e.key === "Esc") {
+    closeModalFn();
+  }
+});
+
+// Close modal with click outside
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    closeModalFn();
+  }
+});
+
+function closeModalFn() {
+  modal.style.display = "none";
+}
